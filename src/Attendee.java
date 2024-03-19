dd852c58470b827130163567553a0a62adb0d001
YinXinYe <x00217230145@njit.edu.cn>
2024-03-19 18:42:32 +0800

    event3.19

--- src/Attendee.java
+++ src/Attendee.java
@@ -1,53 +1,68 @@
 public class Attendee {
-    
     private String name;
     private char gender;
     private String email;
     private int age;
+    private String seatNo;
 
-    public Attendee(String name, char gender, String email, int age) {
+    public Attendee(String name, char gender, String email, int age, String seatNo) {
         this.name = name;
         this.gender = gender;
         this.email = email;
         this.age = age;
-    }
-    
-    public void setName(String name) {
-        this.name = name;
+        this.seatNo = seatNo;
     }
 
-    public void setGender(char gender) {
-        this.gender = gender;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+    this.name = name;
     }
 
     public char getGender() {
         return gender;
     }
 
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
     public String getEmail() {
         return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public int getAge() {
         return age;
     }
 
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSeatNo() {
+        return seatNo;
+    }
+
+    public void setSeatNo(String seatNo) {
+        this.seatNo = seatNo;
+    }
+
     @Override
     public String toString() {
-        return "Name: " + this.name + ", Gender: " + this.gender +
-               ", Email: " + this.email + ", Age: " + this.age;
+        return "Attendee{" +
+                "name='" + name + '\'' +
+                ", gender=" + gender +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                ", seatNo='" + seatNo + '\'' +
+                '}';
     }
 }
 
